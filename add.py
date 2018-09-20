f6dc5a3a4ade08381a056ea05a8c209033c22612
varshaammucmr <43023417+varshaammucmr@users.noreply.github.com>
2018-09-20 08:54:28 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,12 @@
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+#add a anb b
+c=a+b
+print(a,"+",b,"=",c)
+
+
+dl211@soetcse:~/varsha$ python add.py
+enter value of a67
+enter value of b56
+(67, '+', 56, '=', 123)
+
