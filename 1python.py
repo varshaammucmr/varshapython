3d7dc3ce682f18f00cff25b620b413a05e86bbd7
varshaammucmr <43023417+varshaammucmr@users.noreply.github.com>
2018-09-22 20:33:19 +0530

    Update 1python.py

--- 1python.py
+++ 1python.py
@@ -1,9 +1,16 @@
+   MULTIPLICATION :
+
+    INPUT
+
 a=int(input("enter value of a"))
 b=int(input("enter value of b"))
 #add a and b
 c=a*b
 print(a,"*",b,"=",c)
 
+
+
+    OUTPUT
 
 dl211@soetcse:~/varsha$ python3 1python.py
 enter value of a23
