128fda379b1ea766a1b6200c791c77a3c3e39386
varshaammucmr <43023417+varshaammucmr@users.noreply.github.com>
2018-09-23 18:52:49 +0530

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,21 +1,16 @@
-dl211@soetcse:~/varsha$ python multiply.py
-(3, '*', 1, '=', 3)
-(3, '*', 2, '=', 6)
-(3, '*', 3, '=', 9)
-(3, '*', 4, '=', 12)
-(3, '*', 5, '=', 15)
-(3, '*', 6, '=', 18)
-(3, '*', 7, '=', 21)
-(3, '*', 8, '=', 24)
-(3, '*', 9, '=', 27)
-(3, '*', 10, '=', 30)
-dl211@soetcse:~/varsha$ 
+  
+3 TABLE USING WHILE LOOP:
+	
+  INPUT
 
 x=3
 y=1
 while(y<=10):
 	print(x,"*",y,"=",x*y)
 	y=y+1
+
+  OUTPUT
+
 dl211@soetcse:~/varsha$ python multiply.py
 (3, '*', 1, '=', 3)
 (3, '*', 2, '=', 6)
