1b9e06c656c0607d9036fc116d83f52a1e9d4dd8
varshaammucmr <43023417+varshaammucmr@users.noreply.github.com>
2018-10-09 13:00:01 +0530

    Add files via upload

--- tik.py
+++ tik.py
@@ -47,4 +47,321 @@ while True:
 		print("you have entered an invalid position")
 		continue
 
-			
+			dl211@soetcse:~/varsha$ python3 tik.py
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+choose an available place :5
+player 1 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | x | 6 |
+-------------
+| 7 | 8 | 9 |
+choose an available place :6
+player 2 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | x | o |
+-------------
+| 7 | 8 | 9 |
+choose an available place :3
+player 1 >>
+| 1 | 2 | x |
+-------------
+| 4 | x | o |
+-------------
+| 7 | 8 | 9 |
+choose an available place :6
+| 1 | 2 | x |
+-------------
+| 4 | x | o |
+-------------
+| 7 | 8 | 9 |
+choose an available place :8       
+player 2 >>
+| 1 | 2 | x |
+-------------
+| 4 | x | o |
+-------------
+| 7 | o | 9 |
+choose an available place :^Z
+[2]+  Stopped                 python3 tik.py
+dl211@soetcse:~/varsha$ python3 tik.py
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+choose an available place :4
+player 1 >>
+| 1 | 2 | 3 |
+-------------
+| x | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+choose an available place :8
+player 2 >>
+| 1 | 2 | 3 |
+-------------
+| x | 5 | 6 |
+-------------
+| 7 | o | 9 |
+choose an available place :5
+player 1 >>
+| 1 | 2 | 3 |
+-------------
+| x | x | 6 |
+-------------
+| 7 | o | 9 |
+choose an available place :3
+player 2 >>
+| 1 | 2 | o |
+-------------
+| x | x | 6 |
+-------------
+| 7 | o | 9 |
+choose an available place :6
+player 1 >>
+game over
+dl211@soetcse:~/varsha$ python3 tik.py
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+choose an available place :8
+player 1 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | x | 9 |
+choose an available place :6
+player 2 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | o |
+-------------
+| 7 | x | 9 |
+choose an available place :5
+player 1 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | x | o |
+-------------
+| 7 | x | 9 |
+choose an available place :3
+player 2 >>
+| 1 | 2 | o |
+-------------
+| 4 | x | o |
+-------------
+| 7 | x | 9 |
+choose an available place :5
+| 1 | 2 | o |
+-------------
+| 4 | x | o |
+-------------
+| 7 | x | 9 |
+choose an available place :9
+player 1 >>
+| 1 | 2 | o |
+-------------
+| 4 | x | o |
+-------------
+| 7 | x | x |
+choose an available place :2
+player 2 >>
+| 1 | o | o |
+-------------
+| 4 | x | o |
+-------------
+| 7 | x | x |
+choose an available place :7
+player 1 >>
+game over
+dl211@soetcse:~/varsha$ python3 tik.py
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+choose an available place :7
+player 1 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| x | 8 | 9 |
+choose an available place :9
+player 2 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| x | 8 | o |
+choose an available place :5
+player 1 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | x | 6 |
+-------------
+| x | 8 | o |
+choose an available place :6
+player 2 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | x | o |
+-------------
+| x | 8 | o |
+choose an available place :3
+player 1 >>
+| 1 | 2 | x |
+-------------
+| 4 | x | o |
+-------------
+| x | 8 | o |
+choose an available place :8
+player 2 >>
+| 1 | 2 | x |
+-------------
+| 4 | x | o |
+-------------
+| x | o | o |
+choose an available place :8
+| 1 | 2 | x |
+-------------
+| 4 | x | o |
+-------------
+| x | o | o |
+choose an available place :2
+player 1 >>
+| 1 | x | x |
+-------------
+| 4 | x | o |
+-------------
+| x | o | o |
+choose an available place :4
+player 2 >>
+| 1 | x | x |
+-------------
+| o | x | o |
+-------------
+| x | o | o |
+choose an available place :1
+player 1 >>
+game over
+dl211@soetcse:~/varsha$ python3 tik.py
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+choose an available place :7
+player 1 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| x | 8 | 9 |
+choose an available place :9
+player 2 >>
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| x | 8 | o |
+choose an available place :3
+player 1 >>
+| 1 | 2 | x |
+-------------
+| 4 | 5 | 6 |
+-------------
+| x | 8 | o |
+choose an available place :5
+player 2 >>
+| 1 | 2 | x |
+-------------
+| 4 | o | 6 |
+-------------
+| x | 8 | o |
+choose an available place :6
+player 1 >>
+| 1 | 2 | x |
+-------------
+| 4 | o | x |
+-------------
+| x | 8 | o |
+choose an available place :1
+player 2 >>
+game over
+dl211@soetcse:~/varsha$ python3 tik.py
+  File "tik.py", line 42
+    else:
+        ^
+IndentationError: unindent does not match any outer indentation level
+dl211@soetcse:~/varsha$ python3 tik.py
+  File "tik.py", line 42
+    else:
+        ^
+IndentationError: unindent does not match any outer indentation level
+dl211@soetcse:~/varsha$ python3 tik.py
+  File "tik.py", line 42
+    else;
+        ^
+IndentationError: unindent does not match any outer indentation level
+dl211@soetcse:~/varsha$ python3 tik.py
+  File "tik.py", line 42
+    else
+       ^
+IndentationError: unindent does not match any outer indentation level
+dl211@soetcse:~/varsha$ python3 tik.py
+  File "tik.py", line 42
+    else:
+        ^
+IndentationError: unindent does not match any outer indentation level
+dl211@soetcse:~/varsha$ python3 tik.py
+  File "tik.py", line 42
+    else:
+        ^
+TabError: inconsistent use of tabs and spaces in indentation
+dl211@soetcse:~/varsha$ python3 tik.py
+  File "tik.py", line 42
+    else
+       ^
+TabError: inconsistent use of tabs and spaces in indentation
+dl211@soetcse:~/varsha$ python3 tik.py
+  File "tik.py", line 42
+    else:
+       ^
+SyntaxError: invalid syntax
+dl211@soetcse:~/varsha$ 
+
