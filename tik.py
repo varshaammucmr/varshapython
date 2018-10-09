8dc37fffd352a3d1dac83b16470db9c9d34a0015
varshaammucmr <43023417+varshaammucmr@users.noreply.github.com>
2018-10-09 12:52:37 +0530

    Add files via upload

---
+++ tik.py
@@ -0,0 +1,50 @@
+a=['1','2','3','4','5','6','7','8','9']
+def playboard():
+	print('|',a[0],'|',a[1],'|',a[2],'|')
+	print("-------------")
+	print('|',a[3],'|',a[4],'|',a[5],'|')
+	print("-------------")
+	print('|',a[6],'|',a[7],'|',a[8],'|')
+playboard()
+
+playerOneTurn = True
+while True:
+	playboard()
+	p=input("choose an available place :")
+
+	if(p in a):
+		if(a[int(p)-1]=='x' or a[int(p)-1]=='o'):
+			print("place taken, choose another place...")
+			continue
+		else:
+			if playerOneTurn:
+				print("player 1 >>")
+				a[int(p)-1]='x'
+				playerOneTurn= not playerOneTurn
+			else:
+				print("player 2 >>")
+				a[int(p)-1]='o'
+				playerOneTurn = not playerOneTurn
+			for i in(0,3,6):
+				if(a[i]==a[i+1] and a[i]==a[i+2]):
+					print("game over");
+					exit()
+			for i in range(3):
+				if(a[i]==a[i+3] and a[i]==a[i+6]):
+					print("game over...")
+					exit()
+			if(a[0]==a[4] and a[0]==a[8]):
+				print("game over");
+				exit()
+			if(a[0]==a[4] and a[2]==a[6]):
+				print("game over")
+				exit()
+		else:
+			print("Tie")
+			exit()
+
+	else:
+		print("you have entered an invalid position")
+		continue
+
+			
---
+++ tt.py
@@ -0,0 +1,17 @@
+a=[1,2,3,4,5,6,7,8,9]
+for i in a:
+	print(i)
+
+
+dl211@soetcse:~/varsha$ python3 tt.py
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+
