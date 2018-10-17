81c374495744eb7587769f265ac93512189d8adb
varshaammucmr <43023417+varshaammucmr@users.noreply.github.com>
2018-10-17 11:50:52 +0530

    Add files via upload

---
+++ smtp.py
@@ -0,0 +1,15 @@
+
+import smtplib
+s=smtplib.SMTP('smtp.gmail.com',587)
+s.starttls()
+s.login("varshapn27@gmail.com","Va!(^%shApn")
+msg="hello"
+s.sendmail("varshapn27@gmail.com","vadisenays@gmail.com",msg)
+print('sent')
+s.quit()
+
+
+
+dl211@soetcse:~/varsha$ python3 smtp.py
+sent
+
